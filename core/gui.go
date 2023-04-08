b761ae52b32af3c4696e3fcc2dc285948d4c0890
eddiechan <eddiechan@163.com>
2023-04-09 00:08:57 +0800

    优化结构

--- core/gui.go
+++ core/gui.go
@@ -13,10 +13,11 @@ import (
 )
 
 type gui struct {
-	infinite                *widget.ProgressBarInfinite
-	createImgBtn, submitBtn *widget.Button
-	msg                     *strings.Builder
-	app                     fyne.App
+	infinite                          *widget.ProgressBarInfinite
+	createImgBtn, submitBtn, cleanBtn *widget.Button
+	content, input                    *widget.Entry
+	msg                               *strings.Builder
+	app                               fyne.App
 }
 
 func InitGui() {
@@ -28,10 +29,21 @@ func InitGui() {
 	subBtn := widget.NewButton("提交问题", nil)
 	//生成图片按钮
 	createBtn := widget.NewButton("AI图片生成", nil)
+	//内容展示
+	label := widget.NewMultiLineEntry()
+	label.Wrapping = fyne.TextWrapBreak //文字自动换行
+	//输入input
+	input := widget.NewEntry()
+	input.SetPlaceHolder("输入问题/图片内容描述")
+	//清空按钮
+	clearBtn := widget.NewButton("清空", nil)
 	ins := &gui{
 		infinite:     infinite,
 		createImgBtn: createBtn,
 		submitBtn:    subBtn,
+		cleanBtn:     clearBtn,
+		content:      label,
+		input:        input,
 		msg:          &strings.Builder{},
 		app:          myApp,
 	}
@@ -41,75 +53,13 @@ func InitGui() {
 
 func (this_ *gui) show() {
 	myWindow := this_.app.NewWindow("GPT Chat")
-	//内容展示
-	label := widget.NewMultiLineEntry()
-	label.Wrapping = fyne.TextWrapBreak //文字自动换行
-	//输入input
-	input := widget.NewEntry()
-	input.SetPlaceHolder("输入问题/图片内容描述")
-	//清空按钮
-	clearBtn := widget.NewButton("清空", func() {
-		label.SetText("")
-		label.Refresh()
-		openai.ClearAsk()
-		this_.msg.Reset()
-	})
-	this_.submitBtn.OnTapped = func() {
-		go func() {
-			ask := "我:" + input.Text
-			oldContent := this_.msg.String()
-			if oldContent == "" {
-				//第一次
-				this_.msg.WriteString(ask)
-			} else {
-				this_.msg.WriteString("\n" + ask)
-			}
-			label.SetText(this_.msg.String())
-			this_.infinite.Show()
-			this_.msg.WriteString("\n")
-			openai.AskGptStream(openai.GetAskContent(ask), func(answer string, err error) {
-				if err != nil {
-					answer = fmt.Sprintf("GPT ERROR %v", err)
-					this_.msg.WriteString(answer)
-					return
-				}
-				this_.msg.WriteString(answer)
-				label.SetText(this_.msg.String())
-			})
-			this_.infinite.Hide()
-			this_.msg.WriteString("\n---------------------------------------------------------")
-			label.SetText(this_.msg.String())
-		}()
-	}
-	this_.createImgBtn.OnTapped = func() {
-		if input.Text == "" {
-			return
-		}
-		go func() {
-			this_.infinite.Show()
-			imageUrl, err := openai.CreateImgUrl(input.Text)
-			if imageUrl != "" {
-				//fmt.Println(imageUrl)
-				url, _ := storage.ParseURI(imageUrl)
-				w := fyne.CurrentApp().NewWindow("图片")
-				w.SetContent(canvas.NewImageFromURI(url))
-				w.Resize(fyne.NewSize(500, 500))
-				//w.SetFixedSize(true)
-				w.Show()
-			}
-			if err != nil {
-				answer := fmt.Sprintf("CreateIMG ERROR %v", err)
-				this_.msg.WriteString("\n" + answer)
-				this_.msg.WriteString("\n---------------------------------------------------------")
-				label.SetText(this_.msg.String())
-			}
-			this_.infinite.Hide()
-		}()
-	}
+	this_.initCleanBtn()
+	this_.initSubmitBtn()
+	this_.initCreateImgBtn()
 	//布局
-	btnBorders := container.NewBorder(nil, nil, clearBtn, this_.createImgBtn, this_.submitBtn)
-	content := container.NewVBox(input, btnBorders)
-	border := container.NewBorder(content, this_.infinite, nil, nil, label)
+	btnBorders := container.NewBorder(nil, nil, this_.cleanBtn, this_.createImgBtn, this_.submitBtn)
+	content := container.NewVBox(this_.input, btnBorders)
+	border := container.NewBorder(content, this_.infinite, nil, nil, this_.content)
 
 	myWindow.SetContent(border)
 	myWindow.Resize(fyne.NewSize(600, 600))
@@ -130,4 +80,71 @@ func (this_ *gui) isLockBtn() {
 			}
 		}
 	}()
+}
+
+func (this_ *gui) initSubmitBtn() {
+	this_.submitBtn.OnTapped = func() {
+		go func() {
+			ask := "我:" + this_.input.Text
+			oldContent := this_.msg.String()
+			if oldContent == "" {
+				//第一次
+				this_.msg.WriteString(ask)
+			} else {
+				this_.msg.WriteString("\n" + ask)
+			}
+			this_.content.SetText(this_.msg.String())
+			this_.infinite.Show()
+			this_.msg.WriteString("\n")
+			openai.AskGptStream(openai.GetAskContent(ask), func(answer string, err error) {
+				if err != nil {
+					answer = fmt.Sprintf("GPT ERROR %v", err)
+					this_.msg.WriteString(answer)
+					return
+				}
+				this_.msg.WriteString(answer)
+				this_.content.SetText(this_.msg.String())
+			})
+			this_.infinite.Hide()
+			this_.msg.WriteString("\n---------------------------------------------------------")
+			this_.content.SetText(this_.msg.String())
+		}()
+	}
+}
+
+func (this_ *gui) initCreateImgBtn() {
+	this_.createImgBtn.OnTapped = func() {
+		if this_.input.Text == "" {
+			return
+		}
+		go func() {
+			this_.infinite.Show()
+			imageUrl, err := openai.CreateImgUrl(this_.input.Text)
+			if imageUrl != "" {
+				//fmt.Println(imageUrl)
+				url, _ := storage.ParseURI(imageUrl)
+				w := fyne.CurrentApp().NewWindow("图片")
+				w.SetContent(canvas.NewImageFromURI(url))
+				w.Resize(fyne.NewSize(500, 500))
+				//w.SetFixedSize(true)
+				w.Show()
+			}
+			if err != nil {
+				answer := fmt.Sprintf("CreateIMG ERROR %v", err)
+				this_.msg.WriteString("\n" + answer)
+				this_.msg.WriteString("\n---------------------------------------------------------")
+				this_.content.SetText(this_.msg.String())
+			}
+			this_.infinite.Hide()
+		}()
+	}
+}
+
+func (this_ *gui) initCleanBtn() {
+	this_.cleanBtn.OnTapped = func() {
+		this_.content.SetText("")
+		this_.content.Refresh()
+		openai.ClearAsk()
+		this_.msg.Reset()
+	}
 }
